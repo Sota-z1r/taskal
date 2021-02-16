dc4084447a76cb8ca4a1f0ec8053bd84eff8dbc7
Takuya <uoastudent182@gmail.com>
2021-02-16 20:56:48 +0900

    login→indexに遷移できない

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const session = require("express-session");
 
-var indexRouter = require("./routes/index");
+var indexRouter = require("./routes");
 var usersRouter = require("./routes/users");
 const getTodosRoutor = require("./routes/getTodos");
 const addRoutor = require("./routes/add");
 const deleteRoutor = require("./routes/delete");
-const loginRouter = require("./routes/");
+const loginRouter = require("./routes/login");
 
 var app = express();
 
@@ -22,13 +23,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use(
+  session({
+    secret: "secret", //secret属性は指定した文字列を使ってクッキーIDを暗号化しクッキーIDが書き換えらているかを判断する。
+    resave: false, //resaveはセッションにアクセスすると上書きされるオプションらしい。今回はfalse.
+    saveUninitialized: false, //saveUninitializedは未初期化状態のセッションも保存するようなオプション。今回はfalse.
+    cookie: {
+      httpOnly: true, //クライアント側でクッキー値を見れない、書きかえれないようにするオプション
+      secure: true, //https通信
+      maxage: 1000 * 60 * 30, //セッションの消滅時間
+    },
+  })
+  );
+  
 app.use("/", indexRouter);
+app.use("/login", loginRouter);
 app.use("/users", usersRouter);
+
 app.get("/gettodos", getTodosRoutor);
-app.get("/login",loginRouter);
 app.post("/add", addRoutor);
 app.post("/delete/:todoid", deleteRoutor);
+// app.get("/login", loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,40 @@
 const express = require('express');
+const mysql = require('mysql');
+const { render } = require('../app');
+
 const router = express.Router();
 
-router.get('/login', (req,res,next) => {
-  res.render('/views/login.ejs', {title: 'ログイン'});
+const connection = mysql.createConnection({
+  host: 'us-cdbr-east-03.cleardb.com',
+  port: 3306,
+  user: 'b398803bdf1570',
+  password: '8ae8b4f2',
+  database: 'heroku_27791ce74a042e7'
 });
+
+router.get('/',(req, res, next) => {
+  res.render('login', {title: 'ログイン'});
+});
+
+router.post('/', (req,res,next) => {
+  const email = req.body.email;
+  const sql = "SELECT * FROM users WHERE email = ?";
+  connection.query(sql, email, (error, results) => {
+    if(results.length > 0){
+      if(req.body.password === results[0].password){
+        console.log('ログイン成功！');
+        res.redirect('/index', {title: 'ログイン'});
+      }else {
+        console.log('ログイン失敗!');
+        res.redirect('login', {title: 'ログイン'});
+      }
+    } else {
+      console.log('入力しろ');
+      res.redirect('login', {title: 'ログイン'});
+    }
+  });
+});
+
+
 
 module.exports = router;
