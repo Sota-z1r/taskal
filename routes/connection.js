c83f981bc8b62d0e40a9c3c7b0678cd7e4ade17c
matallow <akoegpjmdm@icloud.com>
2021-02-22 17:19:06 +0900

    connection.jsの追加

---
+++ routes/connection.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "us-cdbr-east-03.cleardb.com",
+  port: 3306,
+  user: "b398803bdf1570",
+  password: "8ae8b4f2",
+  database: "heroku_27791ce74a042e7",
+});
+
+module.exports = connection;
